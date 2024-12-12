c390b04a235f7b466dcf619a96c65892ce0761dd
olmos-dev <albrtho94@gmail.com>
2024-12-11 20:45:24 -0600

    agregar estilo show tasks con tailwind css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css">
     @yield('styles')
 </head>
 <body class="container mx-auto mt-10 mb-10 max-w-lg">
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -4,35 +4,41 @@
 
 @section('content')
 
+    <a href="{{route("task.index")}}" class="font-medium text-gray-700 underline decoration-pink-500 "> <i class="fas fa-arrow-left"></i> Volver</a>
+
     @if (session()->has('success'))
         <div>
             {{session('success')}}
         </div>
     @endif
 
-    <p>{{$task->description}}</p>
+    <p class="mb-4 text-slate-700">{{$task->description}}</p>
     @if ($task->long_description)
-        <p>{{$task->long_description}}</p>
+        <p class="mb-4 text-slate-700">{{$task->long_description}}</p>
     @endif
-    <p>{{$task->created_at}}</p>
-    <p>{{$task->updated_at}}</p>
-    <p>{{$task->completed ? 'Completado' : 'No completado'}}</p>
+    <p class="mb-4 text-sm text-slate-500"> creado {{$task->created_at->diffForHumans()}} - actualizado {{$task->updated_at->diffForHumans()}}</p>
 
-    <div>
-        <a href="{{route('task.edit', ['task' => $task->id]) }}">Editar</a>
-    </div>
-    <div>
+    <p class="mb-4">
+        @if ($task->completed)
+            <span class="font-medium text-green-500">Completada</span>
+        @else
+            <span class="font-medium text-red-500">No Completada</span>
+        @endif
+    </p>
+
+    <div class="flex gap-4">
+        <a href="{{route('task.edit', ['task' => $task->id]) }}" class="rounded-md px-2 py-1 text-center font-medium text-slate-700 shadow-sm ring-1 ring-slate-700/10 hover: bg-slate-50">Editar</a>
+
         <form action="{{ route('task.destroy', ['task' => $task->id]) }}" method="post">
             @csrf
             @method('delete')
-            <button type="submit">Eliminar</button>
+            <button type="submit" class="rounded-md px-2 py-1 text-center font-medium text-slate-700 shadow-sm ring-1 ring-slate-700/10 hover: bg-slate-50">Eliminar</button>
         </form>
-    </div>
-    <div>
+
         <form action="{{route('task.toggle', ['task' => $task]) }}" method="post">
             @csrf
             @method('put')
-            <button type="submit"> Marcar como {{$task->completed ? 'no completado' : 'completado'}}</button>
+            <button type="submit" class="rounded-md px-2 py-1 text-center font-medium text-slate-700 shadow-sm ring-1 ring-slate-700/10 hover: bg-slate-50"> Marcar como {{$task->completed ? 'no completado' : 'completado'}}</button>
         </form>
     </div>
 
